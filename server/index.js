e812a50f8c7bf3154e583311b9aefb9306c4c55b
Dos Santos Mickael <mdossan.perso@gmail.com>
2018-05-23 11:20:08 +0200

    Instanciate handler cats before use

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const Server = require('./core/Server');
 
+// Routes
 const routes = require('./routes.json');
+
+// Handlers
+const Cats = require('./handlers/Cats');
 const handlers = {
-    Cats: require('./handlers/Cats')
+    Cats: new Cats()
 };
 
 let server = new Server(express, routes, handlers);
