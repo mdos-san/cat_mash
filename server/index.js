ea74ae00cafe51f80c75860ef0b460dcc341f552
Dos Santos Mickael <mdossan.perso@gmail.com>
2018-05-23 12:34:33 +0200

    Add ressources in index.js

--- server/index.js
+++ server/index.js
@@ -4,10 +4,13 @@ const Server = require('./core/Server');
 // Routes
 const routes = require('./routes.json');
 
+// Ressources
+const cats = require('./cats.json');
+
 // Handlers
 const Cats = require('./handlers/Cats');
 const handlers = {
-    Cats: new Cats()
+    Cats: new Cats(cats.images)
 };
 
 let server = new Server(express, routes, handlers);
