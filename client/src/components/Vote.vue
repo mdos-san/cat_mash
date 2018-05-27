63cf23171a7af04d10e91674408fdecade57c70e
Dos Santos Mickael <mdossan.perso@gmail.com>
2018-05-27 11:54:34 +0200

    Display vote

--- client/src/components/Vote.vue
+++ client/src/components/Vote.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="vote">
       <h1>Vote</h1>
+      <div v-for="cat in cats" :key="cat.id">
+        <img :src="cat.url"/>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Vote',
+  data: () => {
+    return {
+      cats: []
+    }
+  },
   created: async function () {
     const response = await fetch('http://localhost:8081/pair')
-    console.log('Pair', response)
+    const json = await response.json()
+    this.cats = json
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+img {
+  max-width: 100%;
+}
+
+div {
+  width: 50%;
+  display: inline-block;
+}
 </style>
