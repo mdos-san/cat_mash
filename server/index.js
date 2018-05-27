31f685152cf317f42d3ff3ee45555bb6fd318671
Dos Santos Mickael <mdossan.perso@gmail.com>
2018-05-27 13:28:51 +0200

    Inject psql into VoteHandler

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const Server = require('./core/Server');
 const bodyparser = require('body-parser');
 
+// Config
+const psql = require('./config/db');
+
 // Routes
 const routes = require('./routes.json');
 
@@ -11,7 +14,7 @@ const cats = require('./cats.json');
 // Handlers
 const VoteHandler = require('./handlers/Vote');
 const handlers = {
-    Vote: new VoteHandler(cats.images)
+    Vote: new VoteHandler(cats.images, psql)
 };
 
 let server = new Server(express, routes, handlers, bodyparser);
